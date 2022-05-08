2fbb4f70229a5a1c40e29657688abed29227dcd3
Falitac <fastkonrad@gmail.com>
2022-05-08 19:02:39 +0200

    copy n-times

--- main.cc
+++ main.cc
@@ -3,15 +3,20 @@
 struct Node {
   Node(int val = 0)
   : val(val)
+  , next(nullptr)
   { }
   int val;
   Node* next;
 };
 
-void addNode(Node*& head, int val) {
+void add(Node*& head, int val) {
   Node* p = new Node(val);
   p->next = head;
   head = p;
+}
+
+void addAfter(Node* node, int val) {
+  node->next = new Node(val);
 }
 
 void print(Node* head) {
@@ -32,12 +37,38 @@ void clear(Node* head) {
   }
 }
 
+void copyNTimes(Node* head, int n) {
+  if(n <= 0) {
+    return;
+  }
+  if(!head) {
+    return;
+  }
+
+  int listSize = 1;
+  Node* lastElement = head;
+
+  while(lastElement->next) {
+    lastElement = lastElement->next;
+    listSize++;
+  }
+
+  for(int i = 0; i < n * listSize; i++) {
+    addAfter(lastElement, head->val);
+    head = head->next;
+    lastElement = lastElement->next;
+  }
+
+  std::printf("Size = %d\n", listSize);
+}
+
 int main(int argc, char** argv) {
   Node* head;
-  addNode(head, 3);
-  addNode(head, 5);
-  addNode(head, 2);
-  addNode(head, 8);
+
+  add(head, 4);
+  add(head, 2);
+  add(head, 7);
+  copyNTimes(head, 2);
   print(head);
 
   clear(head);
